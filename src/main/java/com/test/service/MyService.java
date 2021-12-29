2755f7e2ec6c558311a873138219592adc9febb6
Necronomicon666 <50944065+Necronomicon666@users.noreply.github.com>
2021-12-29 16:35:42 +0100

    test

---
+++ src/main/java/com/test/service/MyService.java
@@ -0,0 +1,29 @@
+package com.test.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.SecurityContext;
+import javax.annotation.security.RolesAllowed;
+
+import org.keycloak.KeycloakPrincipal;
+
+@Path("/")
+public class MyService {
+    @Context
+    SecurityContext context;
+
+	@GET
+	@Path("hello")
+	@RolesAllowed({"wildfly_role"})
+
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello(){
+		KeycloakPrincipal principal = (KeycloakPrincipal) context.getUserPrincipal();
+		String output = "Hello "+principal.getKeycloakSecurityContext().getToken().getPreferredUsername();
+		return output;
+	}
+
+}
